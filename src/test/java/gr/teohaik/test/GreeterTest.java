6d5117dde43d5f2390fcf914abee4f59df8ea49f
Theodore Chaikalis <teohaik@gmail.com>
2018-09-03 14:56:31 +0300

    initial commit

---
+++ src/test/java/gr/teohaik/test/GreeterTest.java
@@ -0,0 +1,36 @@
+package gr.teohaik.test;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+@RunWith(Arquillian.class)
+public class GreeterTest {
+
+	@Deployment
+	public static JavaArchive createDeployment() {
+		JavaArchive jar = ShrinkWrap.create( JavaArchive.class, "GreterTest.jar")
+				.addClass(Greeter.class)
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+
+		System.out.println(jar.toString(true));
+		return jar;
+	}
+
+	@Inject
+	Greeter greeter;
+
+	@Test
+	public void should_create_greeting() {
+		Assert.assertEquals("Hello, Earthling!",
+				greeter.createGreeting("Earthling"));
+		greeter.greet(System.out, "Earthling");
+	}
+}
